9346d41dfb482fa0cd0d9d3a9ef8a410b95703a1
saltedcake <praveenkumrpk2007@gmail.com>
2024-03-07 18:15:54 -0500

    Added toggle switch
    Added toggle switch

--- MindReader.py
+++ MindReader.py
@@ -12,18 +12,20 @@ BAUD_RATE = 9600
 # Open the serial port
 ser = serial.Serial(HAND_CONTROLLER_COM_PORT, BAUD_RATE)
 
+ENABLE_BLINK = False
+ENABLE_ATTENTION = not ENABLE_BLINK
 
-BLINK_STREANGTH_GREATER_THAN = 10
-blink_count = 0
+
+
+BLINK_THRESHOLD = 10
 blink_start_time = time.time()
-blink_interval = 10000  # 2-second interval for counting blinks
+blink_interval = 5  # 2-second interval for counting blinks
 blink_window = deque(maxlen=10)  # Window size for storing blink data
 
-ATTENTION_STREANGTH_GREATER_THAN = 10
+ATTENTION_THRESHOLD = 10
 ATTENTION_COUNT_GREATER_THAN = 3
 attention_start_time = time.time()
-attention_count = 0
-attention_interval = 100000  # 5-second interval for counting attention data
+attention_interval = 10  # 5-second interval for counting attention data
 attention_window = deque(maxlen=10)  # Window size for storing attention data
 
 
@@ -37,36 +39,39 @@ def parse_json(data):
     try:
         json_data = json.loads(data)
          # Do whatever you want with the JSON data here
-        if 'eSense' in json_data:
-            if json_data['eSense']['attention'] > ATTENTION_STREANGTH_GREATER_THAN :
-                attention_window.append(1)
-                print(json_data['eSense'])
+        if ENABLE_ATTENTION:
+            if 'eSense' in json_data:
+                if json_data['eSense']['attention'] > ATTENTION_THRESHOLD :
+                    attention_window.append(1)
+                    print(json_data['eSense'])
 
-        if time.time() - attention_start_time >= attention_interval:
-            attention_count = sum(attention_window)  # Sum of 1's in the window
-            print(f"Attention strength greater than {ATTENTION_STREANGTH_GREATER_THAN} count in {attention_interval} seconds: {attention_count}")
-            if attention_count > ATTENTION_COUNT_GREATER_THAN : 
-                ser.write(str(99).encode())
-                time.sleep(10)
-            attention_start_time = time.time()  # Reset the start time
-            attention_window.clear()  # Clear the window for the next interval
+            if time.time() - attention_start_time >= attention_interval:
+                attention_count = sum(attention_window)  # Sum of 1's in the window
+                print(f"Attention strength greater than {ATTENTION_THRESHOLD} count in {attention_interval} seconds: {attention_count}")
+                if attention_count > ATTENTION_COUNT_GREATER_THAN :
+                    print(f"******Finger wave pattern initiated...........")  
+                    ser.write(str(99).encode())
+                    time.sleep(10)
+                attention_start_time = time.time()  # Reset the start time
+                attention_window.clear()  # Clear the window for the next interval
 
+        if ENABLE_BLINK:
+            if 'blinkStrength' in json_data:
+                if json_data['blinkStrength'] > BLINK_THRESHOLD : 
+                    blink_window.append(1)  # Append a '1' to the window if blink is detected
+                    print(json_data)
 
-        if 'blinkStrength' in json_data:
-            if json_data['blinkStrength'] > BLINK_STREANGTH_GREATER_THAN : 
-                blink_window.append(1)  # Append a '1' to the window if blink is detected
-                print(json_data)
-
-        if time.time() - blink_start_time >= blink_interval:
-            blink_count = sum(blink_window)  # Sum of 1's in the window
-            print(f"Blink strength greater than {BLINK_STREANGTH_GREATER_THAN} count in {blink_interval} seconds: {blink_count}")
-            if blink_count > 0 : 
-                if blink_count > 5 : 
-                    blink_count = 5
-                ser.write(str(blink_count).encode())
-                time.sleep(5)
-            blink_start_time = time.time()  # Reset the start time
-            blink_window.clear()  # Clear the window for the next interval
+            if time.time() - blink_start_time >= blink_interval:
+                blink_count = sum(blink_window)  # Sum of 1's in the window
+                print(f"Blink strength greater than {BLINK_THRESHOLD} count in {blink_interval} seconds: {blink_count}")
+                if blink_count > 0 : 
+                    if blink_count > 5 : 
+                        blink_count = 5
+                    print(f"******Arm movement initiated showing {blink_count}........") 
+                    ser.write(str(blink_count).encode())
+                    time.sleep(5)
+                blink_start_time = time.time()  # Reset the start time
+                blink_window.clear()  # Clear the window for the next interval
 
     except json.JSONDecodeError as e:
         #print("Error decoding JSON:", e)
@@ -89,4 +94,4 @@ try:
     time.sleep(5)
     client.close()
 except socket.error as se:
-    pass
+    print("Error:", se)
---
+++ monitor.py
@@ -0,0 +1,42 @@
+import socket
+import json
+import time
+
+ENABLE_BLINK = True
+ENABLE_ATTENTION = not ENABLE_BLINK
+
+def parse(data):
+    try:
+
+        json_data = json.loads(data)
+
+        if ENABLE_ATTENTION:
+            if 'eSense' in json_data:
+                print("Attention value: " + str(json_data['eSense']['attention']))
+
+        if ENABLE_BLINK:
+            if('blinkStrength' in json_data):
+                print("Blink value: " + str(json_data['blinkStrength']))           
+
+
+    except:
+        pass
+
+
+my_write_buffer = b'{"enableRawOutput": true, "format": "Json"}'
+
+try:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(("127.0.0.1", 13854))
+    client.send(my_write_buffer)
+    while True:
+        data = client.recv(2048)
+        if not data:
+            break
+        packets = data.decode('utf-8').split('\r')
+        for packet in packets:
+            parse(packet.strip())
+    time.sleep(5)
+    client.close()
+except socket.error as se:
+    print("Error:", se)
